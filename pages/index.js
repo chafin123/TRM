fe4a195024796220a676276158be13e3a7200777
Steven Chafin <71861919+chafin123@users.noreply.github.com>
2023-09-18 14:16:01 -0800

    Chang

--- pages/index.js
+++ pages/index.js
@@ -9,15 +9,16 @@ import Image from 'next/image';
 export default function Menu({ data }) {
     return (
     <div className={styles.menuContainer}>
-
-
-      <div className={styles.entriesContainer}>
-      <div className="pt-10 pb-10">
-        <Image src="/images/logo.png" alt='' width={300} height={300}/>
-      </div>
+      <div className='text-center'>
+        <div className="pt-10 pb-10">
+          <Image src="/images/logo.png" alt='' width={300} height={300}/>
+        </div>
         <div className='pb-10'>
           <h2 className=' text-5xl'>Today's Specials</h2>
         </div>
+      </div>
+
+      <div className={styles.entriesContainer}>
           <Soup props={data} className={styles} moduleChange ={styles}/>
           <Appetizers props={data} className={styles} moduleChange={styles}/>
           <Entrees props={data} className={styles} moduleChange={styles}/>
