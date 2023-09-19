6b99ab8db76a8d01f7350a274ceef920d8207059
Steven Chafin <71861919+chafin123@users.noreply.github.com>
2023-09-18 16:27:19 -0800

    logo size 200

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,8 @@ export default function Menu({ data }) {
     <div className={styles.menuContainer}>
       <div className=" pb-16 text-center w-full before:bg-blend-multiply before:contents-[''] before:absolute before:left-0 before:w-[100rem] before:h-[300px] before:z-[-1] before:bg-[#881b1d]/90">
         <div className="pt-10 flex justify-evenly">
-          <div className="pt-10 pb-16 w-1/3 text-end pr-10">
-            <Image className=' mix-blend-multiply' src="/images/TurkeyRed.svg" alt='' width={150} height={150}/>
+          <div className=" pb-16 w-1/3 text-center">
+            <Image className=' mix-blend-multiply' src="/images/TurkeyRed.svg" alt='' width={200} height={200}/>
           </div>
           <div className='pb-10 pl-2 z-10 self-center w-2/3 text-start'>
             <h2 className='text-[#ECE7DB] text-7xl'>Today's Specials</h2>
